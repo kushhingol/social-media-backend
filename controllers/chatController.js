32389c7f1327273969c864a87747fa78004d4d66
Kush Hingol <kushhingol@Kushs-MacBook-Pro.local>
2024-07-29 17:11:23 +0530

    added get message feature

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -22,6 +22,29 @@ const sendMessage = async (req, res) => {
   res.status(201).json(newMessage);
 };
 
+/**
+ * @desc get  message
+ * @route GET /chat/message/:userId
+ * @access Private
+ */
+const getMessages = async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    const messages = await Message.find({
+      $or: [
+        { senderId: req.user._id, receiverId: userId },
+        { senderId: userId, receiverId: req.user._id },
+      ],
+    }).sort({ createdAt: -1 });
+
+    res.json(messages);
+  } catch (err) {
+    res.status(500).json({ message: "Error retrieving messages" });
+  }
+};
+
 module.exports = {
   sendMessage,
+  getMessages,
 };
